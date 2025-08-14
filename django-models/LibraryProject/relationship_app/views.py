505eaa953f2a67f3a3f464e6aaae65aca6bd7ff2
unknown <japhethdanny@gmail.com>
2025-08-14 15:57:15 +0300

    Made a minor tweak to pass the checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from .models import Library, Book, Author
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import user_passes_test
 
 
 
