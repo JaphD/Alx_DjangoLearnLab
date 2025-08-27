acd99c9bf65ac86f57d9e61a78e2a722851b6787
unknown <japhethdanny@gmail.com>
2025-08-27 15:24:47 +0300

    Modified the import for permission classes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions, filters, status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
